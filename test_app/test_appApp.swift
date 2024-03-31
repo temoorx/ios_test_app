6275282db2bcbfabb24b772b23d5da8294d53c2d
temoorx <taak140@gmail.com>
2024-03-31 16:59:19 +0500

    Initial Commit

---
+++ test_app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test_app
+//
+//  Created by Mac on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
